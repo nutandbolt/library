a79065baf8cb4aada4e43a8dfc583c2e9607f3de
nutandbolt <ajaykrao@gmail.com>
2026-01-04 20:55:18 +0530

    Updated SCOPES to include drive

--- sheets_db.py
+++ sheets_db.py
@@ -3,7 +3,10 @@ import gspread
 from google.oauth2.service_account import Credentials
 from datetime import datetime
 
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+SCOPES = [
+        "https://www.googleapis.com/auth/spreadsheets",
+          "https://www.googleapis.com/auth/drive"
+          ]
 
 creds = Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
